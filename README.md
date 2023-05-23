04c6f6034017eb79737234375d8d3524e392b63b
Alex Blanco <53310027+coolbeansblanco@users.noreply.github.com>
2023-05-22 23:50:55 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # rock-paper-scissors
+Taken from The Odin Project
+
+5/22/2023 - Before starting
+This is a project intended for practice with javascript. The only HTML included will serve to link the javascript file. There will be no CSS. The game will be played exclusively in the console.
+
+The only specified handling for robustness is the ability to accept any method of capitalization from user input.
+
+5/22/2023 - After finishing
+This project was great for practicing javascript. There are many features that would improve the program, but as the main purpose is practice I only included logic to check for inputs that were not the 3 options above.The programming seems to be quite tedious to read but it is a solution nonetheless.
---
+++ script.js
@@ -0,0 +1,84 @@
+function getComputerChoice(){
+    choices = ["Rock", "Paper", "Scissors"];
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function playRound(playerSelection, computerSelection) {
+    
+    //all possible winning scenarios, return win
+    if (playerSelection === "Rock" && computerSelection === "Scissors" ||
+        playerSelection === "Paper" && computerSelection === "Rock" ||
+        playerSelection === "Scissors" && computerSelection === "Paper"
+    )
+        return "win";
+
+    //all possible losing scenarios, return false
+    else if (playerSelection === "Rock" && computerSelection === "Paper" ||
+        playerSelection === "Paper" && computerSelection === "Scissors" ||
+        playerSelection === "Scissors" && computerSelection === "Rock"
+    )
+        return "lose";
+    
+    return "tie";
+}
+
+function game(){
+
+    const finalRound = 5;
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i=0; i<finalRound; i++) {
+
+        let playerSelection = prompt("Rock, paper, scissors, shoot!: ");
+        
+        //Changes first character of playerSelection to uppercase and the rest to lowercase
+        playerSelection = playerSelection.substring(0,1).toUpperCase() + playerSelection.substring(1).toLowerCase();
+    
+        let computerSelection = getComputerChoice();
+        let roundMessage = "Round " + (i+1) + ".";
+        let result;
+
+        //Round will only be played with a valid answer. If one is not provided, result will remain undefined
+        if (playerSelection === "Rock" ||
+            playerSelection === "Paper" || 
+            playerSelection === "Scissors" )
+            result = playRound(playerSelection, computerSelection);
+
+        if (result === "win"){
+            roundMessage += " You win! " + playerSelection + " beats " + computerSelection + ".";
+            playerScore++;
+        }
+
+        else if (result === "lose"){
+            roundMessage += " You lose! " + computerSelection + " beats " + playerSelection + ".";
+            computerScore++;
+        }
+
+        else if (result === "tie")
+            roundMessage += " Tie!"
+
+        else{
+            roundMessage += " " + playerSelection + " is not a valid response. Round will repeat."
+            i--;
+        }
+
+        roundMessage += " Player:" + playerScore + " Computer:" + computerScore;
+        console.log(roundMessage);
+    }
+
+    if (playerScore > computerScore)
+        winnerMessage = "You have won! Congratulations.";
+    else if (playerScore < computerScore)
+        winnerMessage = "You have lost! Better luck next time."
+    else
+        winnerMessage = "You have tied!"
+        
+    winnerMessage += " Final scores - Player:" + playerScore + " Computer:" + computerScore;
+    console.log(winnerMessage);
+}
+
+game();
+
+
+
