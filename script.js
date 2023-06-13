3c6c72d8fd4024ed6851c137272af0c3174e24cd
Alex Blanco <53310027+coolbeansblanco@users.noreply.github.com>
2023-06-13 00:28:42 -0600

    add event listeners and refactor playRound to match

--- script.js
+++ script.js
@@ -2,87 +2,97 @@ const rockBtn = document.querySelector('.rock');
 const paperBtn = document.querySelector('.paper');
 const scissorsBtn = document.querySelector('.scissors');
 
+rockBtn.addEventListener('click', () => playRound("Rock"));
+paperBtn.addEventListener('click', () => playRound("Paper"));
+scissorsBtn.addEventListener('click', () => playRound("Scissors"))
+
 function getComputerChoice(){
     choices = ["Rock", "Paper", "Scissors"];
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
     
+    const computerSelection = getComputerChoice();
+
     //all possible winning scenarios, return win
+    console.log ("Hello " + playerSelection);
     if (playerSelection === "Rock" && computerSelection === "Scissors" ||
         playerSelection === "Paper" && computerSelection === "Rock" ||
         playerSelection === "Scissors" && computerSelection === "Paper"
     )
-        return "win";
+    {
+        console.log("win") ;
+    }
+        
 
     //all possible losing scenarios, return false
     else if (playerSelection === "Rock" && computerSelection === "Paper" ||
         playerSelection === "Paper" && computerSelection === "Scissors" ||
         playerSelection === "Scissors" && computerSelection === "Rock"
     )
-        return "lose";
+        console.log("lose");
     
-    return "tie";
+    console.log("tie");
 }
 
-function game(){
+// function game(){
 
-    const finalRound = 5;
-    let playerScore = 0;
-    let computerScore = 0;
+//     const finalRound = 5;
+//     let playerScore = 0;
+//     let computerScore = 0;
 
-    for (let i=0; i<finalRound; i++) {
+//     for (let i=0; i<finalRound; i++) {
 
-        let playerSelection = prompt("Rock, paper, scissors, shoot!: ");
+//         let playerSelection = prompt("Rock, paper, scissors, shoot!: ");
         
-        //Changes first character of playerSelection to uppercase and the rest to lowercase
-        playerSelection = playerSelection.substring(0,1).toUpperCase() + playerSelection.substring(1).toLowerCase();
+//         //Changes first character of playerSelection to uppercase and the rest to lowercase
+//         playerSelection = playerSelection.substring(0,1).toUpperCase() + playerSelection.substring(1).toLowerCase();
     
-        let computerSelection = getComputerChoice();
-        let roundMessage = "Round " + (i+1) + ".";
-        let result;
+//         let computerSelection = getComputerChoice();
+//         let roundMessage = "Round " + (i+1) + ".";
+//         let result;
 
-        //Round will only be played with a valid answer. If one is not provided, result will remain undefined
-        if (playerSelection === "Rock" ||
-            playerSelection === "Paper" || 
-            playerSelection === "Scissors" )
-            result = playRound(playerSelection, computerSelection);
+//         //Round will only be played with a valid answer. If one is not provided, result will remain undefined
+//         if (playerSelection === "Rock" ||
+//             playerSelection === "Paper" || 
+//             playerSelection === "Scissors" )
+//             result = playRound(playerSelection, computerSelection);
 
-        if (result === "win"){
-            roundMessage += " You win! " + playerSelection + " beats " + computerSelection + ".";
-            playerScore++;
-        }
+//         if (result === "win"){
+//             roundMessage += " You win! " + playerSelection + " beats " + computerSelection + ".";
+//             playerScore++;
+//         }
 
-        else if (result === "lose"){
-            roundMessage += " You lose! " + computerSelection + " beats " + playerSelection + ".";
-            computerScore++;
-        }
+//         else if (result === "lose"){
+//             roundMessage += " You lose! " + computerSelection + " beats " + playerSelection + ".";
+//             computerScore++;
+//         }
 
-        else if (result === "tie")
-            roundMessage += " Tie!"
+//         else if (result === "tie")
+//             roundMessage += " Tie!"
 
-        else{
-            roundMessage += " " + playerSelection + " is not a valid response. Round will repeat."
-            i--;
-        }
+//         else{
+//             roundMessage += " " + playerSelection + " is not a valid response. Round will repeat."
+//             i--;
+//         }
 
-        roundMessage += " Player:" + playerScore + " Computer:" + computerScore;
-        console.log(roundMessage);
-    }
+//         roundMessage += " Player:" + playerScore + " Computer:" + computerScore;
+//         console.log(roundMessage);
+//     }
 
-    if (playerScore > computerScore)
-        winnerMessage = "You have won! Congratulations.";
-    else if (playerScore < computerScore)
-        winnerMessage = "You have lost! Better luck next time."
-    else
-        winnerMessage = "You have tied!"
+//     if (playerScore > computerScore)
+//         winnerMessage = "You have won! Congratulations.";
+//     else if (playerScore < computerScore)
+//         winnerMessage = "You have lost! Better luck next time."
+//     else
+//         winnerMessage = "You have tied!"
         
-    winnerMessage += " Final scores - Player:" + playerScore + " Computer:" + computerScore;
-    console.log(winnerMessage);
-}
+//     winnerMessage += " Final scores - Player:" + playerScore + " Computer:" + computerScore;
+//     console.log(winnerMessage);
+// }
 
-game();
+//game();
 
 
 
